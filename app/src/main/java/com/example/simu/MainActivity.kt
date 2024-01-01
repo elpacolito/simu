631057bea9d2cf8f962d7bda3767c89193d57004
pacotro <pacotro@outlook.fr>
2024-01-01 22:31:36 +0100

    modification du menu

--- app/src/main/java/com/example/simu/MainActivity.kt
+++ app/src/main/java/com/example/simu/MainActivity.kt
@@ -2,8 +2,16 @@ package com.example.simu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.view.View
+import android.content.Intent
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    //Button new, continuer, historique, quitter;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
